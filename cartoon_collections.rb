7f545cf864c44bdffb51e2ca369c08efa6690ee3
Will Jones <willjones2010@gmail.com>
2019-03-22 19:35:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |food| 
     if food == "cheddar" || "gouda" || "camembert"
-      return food
+      return 
     else 
       return nil
 end
