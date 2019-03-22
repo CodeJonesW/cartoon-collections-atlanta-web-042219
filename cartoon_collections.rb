8586da3f16d60e0fda8fe28fb1e963a47449813e
Will Jones <willjones2010@gmail.com>
2019-03-22 19:15:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ end
 return new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.length > 4
 end
 
 def find_the_cheese# code an argument here
