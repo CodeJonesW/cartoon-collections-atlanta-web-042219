127448d9835b9a75cae82e1c22345b308b1b0a74
Will Jones <willjones2010@gmail.com>
2019-03-22 19:30:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
+  i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.any? do |food|
+    food[i] == "cheddar" || "gouda" || "camembert"
 end
