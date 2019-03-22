726ff29903c9f52a8a8da72feb23bc461662d5b7
Will Jones <willjones2010@gmail.com>
2019-03-22 19:27:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,13 +12,14 @@ end
 return new_array
 end
 
-def long_planeteer_calls(array)
-  array.find do |word|
-   if word.length > 4
-    puts true
-  end
-end
-return false
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
 def find_the_cheese# code an argument here
